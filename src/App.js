3cfd15fc774382c7af93033c909fcd8775b3df00
AlbertoPaulPopescu <63293696+AlbertoPaulPopescu@users.noreply.github.com>
2021-06-15 20:16:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { About } from "./pages/about";
 import { UserContext } from "./UserContext";
 
 function AppRouter() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); //this is what I don't get.... 
 
-  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]); // This is isolating the useState to render only when there are changes exclusivleyt to the user
 
   return (
     <Router>
@@ -15,15 +15,15 @@ function AppRouter() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">Home</Link> 
             </li>
             <li>
               <Link to="/about/">About</Link>
             </li>
           </ul>
         </nav>
-        <UserContext.Provider value={value}>
-          <Route path="/" exact component={Index} />
+        <UserContext.Provider value={value}> // This is 'providing' a value that is taken from the user state which is initially null and given to the usercontext in index.js on line 6
+          <Route path="/" exact component={Index} /> // This 'exact' disables some react rule which won't let you go deeper than '/' on the route so next line will also result in you going to the '/' instead of the about page /about/
           <Route path="/about/" component={About} />
         </UserContext.Provider>
       </div>
