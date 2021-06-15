dcd7dc4e8b4a66afa8e047487f0d2dfd5a0527f5
AlbertoPaulPopescu <63293696+AlbertoPaulPopescu@users.noreply.github.com>
2021-06-15 19:49:10 +0100

    Update UserContext.js

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const UserContext = createContext(null);
+export const UserContext = createContext(null); //I mean we already provide a value to the user context from the userstate which is either logged in or logged out but this will probably make sure that if there is no value provided the context is null, might be a convention
